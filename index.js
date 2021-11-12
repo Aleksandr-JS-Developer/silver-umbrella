e5a6d4bf2331959f3177a1ea601e80fdaed0817a
Aleksandr Dukhovnyy <script@vip-person.net>
2021-11-12 11:36:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 if(window.silverUmbrella !== true){
+  const el = document.querySelector('[style="position: absolute; width: 100vw; height: 100vh; top: 0; left: 0;"]');
+  el.deleteAttribute('onmouseenter');
+  el.deleteAttribute('style');
+  
   console.log(1);
 } 
 
